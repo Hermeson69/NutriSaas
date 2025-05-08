d27c8a1a129b70fadf0ac092e58ef8ebd5afc66c
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-08 12:42:39 -0300

    faet: layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+interface LayoutProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export function Layout({ children, className }: LayoutProps) {
+    return (
+        <div className="flex min-dvh-screen">
+            <main
+                className={cn(
+                    "flex-1 p-8",
+                    className
+                )}
+            >
+                {/* Conteúdo da página */}
+                <div>{children}</div>
+            </main>
+        </div>
+    );
+}
