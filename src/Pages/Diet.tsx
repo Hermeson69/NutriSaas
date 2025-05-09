70d6dbebc9b83bbc2daa2a0d0203043d08df5df4
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-08 21:24:18 -0300

    feat: identation

--- src/Pages/Diet.tsx
+++ src/Pages/Diet.tsx
@@ -1,8 +1,6 @@
-
 export const Diet = () => {
   return (
     <>
-
       <h1 className="text-2xl font-bold">Diet</h1>
       <p className="mt-4 text-gray-600">Welcome to your fitness dashboard!</p>
     </>
