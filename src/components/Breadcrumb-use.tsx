2dc45e1506732bb2d150c58c2fa635306a1d0841
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-08 21:00:34 -0300

    feat: import and create a component Breadcrumb for navegation and indicator

---
+++ src/components/Breadcrumb-use.tsx
@@ -0,0 +1,43 @@
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb";
+  import { Link, useLocation } from "react-router-dom";
+  
+  export const BreadcrumbUse = () => {
+    const location = useLocation();
+    const pathSegments = location.pathname
+      .split("/")
+      .filter((segment) => segment);
+  
+    return (
+      <Breadcrumb>
+        <BreadcrumbList>
+          {/* Home */}
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+  
+          {/* Segmentos do caminho */}
+          {pathSegments.map((segment, index) => {
+            const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
+            return (
+              <>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem key={href}>
+                  <BreadcrumbLink asChild>
+                    <Link to={href}>{segment}</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            );
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    );
+  };
