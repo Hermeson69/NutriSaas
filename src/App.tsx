5b19c422c0fab0fec4901d95c752b14ce666be0e
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-09 21:04:37 -0300

    feat: litlle ajustments

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,33 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { SidebarNavigation } from "@/components/sidebar-navigation"
 
 import DashBoard from "@/Pages/DashBoard"
-import  {Gym}  from "@/Pages/Gym"
-import { BreadcrumbUse } from "@/components/Breadcrumb-use"
+import { Gym } from "@/Pages/Gym"
 import { Diet } from "@/Pages/Diet"
 import { Plans } from "@/Pages/Plans"
+import {Profile} from "@/Pages/Profile"
 // import { ThemeProvider } from "@/components/theme-provider"
 import Announcements from "@/Pages/Announcements"
-import { Layout } from "./components/Layout"
+import {Layout} from "@/components/Layout"
 
-
-export default function App(){
+export default function App() {
   return (
     // <ThemeProvider defaultTheme="system">
     <Router>
       <div className="flex min-h-screen">
         <SidebarNavigation />
-      <Layout>
-        <main className="flex-1 p-8">
-          <Routes>
-            <Route path="/" element={<DashBoard />} />
-            <Route path="/Gym" element={<Gym />} />
-            <Route path="/Diet" element={<Diet />} />
-            <Route path="/Plans" element={<Plans />} />
-            <Route path="/Announcements" element={<Announcements />} />
-          </Routes>
-        </main>
-        
-          <BreadcrumbUse/>
-      </Layout>
+        <Layout>
+          <main className="flex-1 p-5">
+            <Routes>
+              <Route path="/" element={<DashBoard />} />
+              <Route path="/Gym" element={<Gym />} />
+              <Route path="/Diet" element={<Diet />} />
+              <Route path="/Plans" element={<Plans />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/Announcements" element={<Announcements />} />
+            </Routes>
+          </main>
+          {/* Removido o BreadcrumbUse daqui */}
+        </Layout>
       </div>
     </Router>
     // </ThemeProvider>
