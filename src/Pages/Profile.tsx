3398841ac4e28b17d90bc04168a07a03dedc69e8
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-09 21:04:21 -0300

    feat: create a temporary profile page not necessarily will state at sidebar wher you click in your profile circule redirect to go her

---
+++ src/Pages/Profile.tsx
@@ -0,0 +1,12 @@
+import { Pageconfig } from "@/components/Pageconfig";
+
+export const Profile = () => {
+  return (
+    <Pageconfig
+      title="Perfil"
+      description="Bem-vindo ao seu painel de controle"
+    >
+      <div>{/* Seus componentes e conteúdo aqui */}</div>
+    </Pageconfig>
+  );
+};
