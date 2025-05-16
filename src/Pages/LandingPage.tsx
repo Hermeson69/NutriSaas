4bd10a4a9be811603911b90d67f7bbac7f2989c3
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-16 09:13:19 -0300

    feat: litlle ajsuts

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -9,10 +9,16 @@ import PricingSection from "@/components/LandingPage/PricingSection";
 import CTASection from "@/components/LandingPage/CTASection";
 import AppDownloadSection from "@/components/LandingPage/AppDownloadSection";
 import "../index.css";
+import { useTheme } from "@/components/Provider/theme-provider";
+import {cn} from "@/lib/utils"
 
 export default function LandingPage() {
+    const { theme } = useTheme();
+    const isDark = theme === "dark";
   return (
-    <div className="flex min-h-screen flex-col items-center">
+    <div className={cn("flex min-h-screen flex-col items-center",
+      isDark ? "bg-bark text-white" : "bg-light-50 text-dark"
+    )}>
       <Header />
       <main className="flex-1">
         <HeroSection />
--- src/components/LandingPage/TrustedBySection.tsx
+++ src/components/LandingPage/TrustedBySection.tsx
@@ -1,10 +1,10 @@
 const TrustedBySection = () => {
   return (
-    <section className="border-y bg-white py-12">
+    <section className="border-y bg-transparent py-12">
       <div className="container">
         <h2 className="text-center text-lg font-medium text-[#666666] mb-8">
           Utilizado por milhares de pessoas em todo o Brasil
-        </h2>
+        </h2>x
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8 items-center justify-items-center opacity-70">
           {[1, 2, 3, 4, 5, 6].map((i) => (
             <img
