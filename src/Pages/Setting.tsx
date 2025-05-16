081c83791d0493985e59a706ed39301ba7c535ae
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-16 10:37:38 -0300

    feat: setting page

---
+++ src/Pages/Setting.tsx
@@ -0,0 +1,10 @@
+import { Pageconfig } from "@/components/Pageconfig"
+
+export const Setting = () =>{
+    <Pageconfig
+          title="Configurações"
+          description="Bem-vindo ao seu painel de controle"
+        >
+          <div>{/* Seus componentes e conteúdo aqui */}</div>
+        </Pageconfig>
+}
