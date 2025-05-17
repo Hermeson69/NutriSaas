194e93b1dc7ee7d09eb7da5d315626209b6e8c80
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-16 22:19:50 -0300

    feat: theme provider im app file

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,36 @@ import { Gym } from "@/Pages/Gym";
 import { Diet } from "@/Pages/Diet";
 import { Plans } from "@/Pages/Plans";
 import { Profile } from "@/Pages/Profile";
-import { ThemeProvider } from "@/components/Provider/theme-provider";
+import { ThemeProvider, useTheme } from "@/components/Provider/theme-provider";
 // import  LandingPage  from "@/Pages/LandingPage"
 import Announcements from "@/Pages/Announcements";
 import { Layout } from "@/components/Layout/Layout";
 import { ThemeToggle } from "./components/theme-toggle";
+import { cn } from "./lib/utils";
 // import  {Setting}  from "./Pages/Setting";
 
 export default function App() {
+    const { theme } = useTheme();
+    const isDark = theme === "dark";
   return (
     <ThemeProvider defaultTheme="system">
       {/* <LandingPage/> */}
       <Router>
-        <div className="flex min-h-screen">
-          <SidebarNavigation />
-          <Layout>
+        <div className={cn("flex min-h-screen",
+          isDark ? "bg-bark text-gray-100" : "bg-light-50 text-dark",
+        )}>
+          <SidebarNavigation className="fixed" />
+          <Layout className="ml-15">
             <div className="flex justify-end mb-4">
               <ThemeToggle size="md" />
             </div>
             <main className="flex-1 p-5 ">
               <Routes>
                 <Route path="/" element={<DashBoard />} />
-                <Route path="/Gym" element={<Gym />} />
-                <Route path="/Diet" element={<Diet />} />
-                <Route path="/Plans" element={<Plans />} />
-                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Treino" element={<Gym />} />
+                <Route path="/Dieta" element={<Diet />} />
+                <Route path="/Planos" element={<Plans />} />
+                <Route path="/Perfil" element={<Profile />} />
                 <Route path="/Announcements" element={<Announcements />} />
                 {/* <Route path="/Setting" element={<Setting />} /> */}
               </Routes>
