1a4bbb5350044edfc538e783139ee79359d01a50
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-16 22:26:49 -0300

    feat: import a two section of diet and gym pages respectively

--- src/Pages/DashBoard.tsx
+++ src/Pages/DashBoard.tsx
@@ -1,12 +1,22 @@
-import { FitnessMetrics } from "@/components/fitness-metrics"
-import { Pageconfig } from "@/components/Pageconfig"
+import FoodTrackingSection from "@/components/Diet/FoodTrackingSection";
+import { FitnessMetrics } from "@/components/fitness-metrics";
+import TrainingTrackingSection from "@/components/Gym/TrainingTrackingSection";
+import { Pageconfig } from "@/components/Pageconfig";
+// import { useTheme } from "@/components/Provider/theme-provider";
+// import { cn } from "@/lib/utils";
 
 export default function DashBoard() {
+  // const { theme } = useTheme();
+  // const isDark = theme === "dark";
   return (
-    <Pageconfig title="Dashboard" description="Bem-vindo ao seu painel de controle">
-      <FitnessMetrics
-      className="flex-1"
-      />
+    <Pageconfig
+      title="Dashboard"
+      description="Bem-vindo ao seu painel de controle"
+      // classNameProp={cn( isDark ? "bg-bark text-gray-100" : "bg-light-50 text-dark")}
+    >
+      <FitnessMetrics className="flex-1" />
+      <FoodTrackingSection />
+      <TrainingTrackingSection/>
     </Pageconfig>
-  )
+  );
 }
