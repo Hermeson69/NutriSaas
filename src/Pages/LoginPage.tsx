dd14f5c9b078002410105c518989d8c724eebe69
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-25 15:46:59 -0300

    test: test

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,14 +1,19 @@
-// import { GalleryVerticalEnd } from "lucide-react"
-
-import { LoginForm } from "@/components/login-form"
-import { SignUpForm } from "@/components/sign-up-form"
+import { useState } from "react";
+import { LoginForm } from "@/components/login-form";
+import { SignUpForm } from "@/components/sign-up-form";
 
 export default function LoginPage() {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <LoginForm />
+        {isLogin ? (
+          <LoginForm onToggleForm={() => setIsLogin(false)} />
+        ) : (
+          <SignUpForm onToggleForm={() => setIsLogin(true)} />
+        )}
       </div>
     </div>
-  )
+  );
 }
