d5f729b2d61988423cc4ead7f5e72996e3dd6fc1
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-26 13:36:57 -0300

    feat: new app ok

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import LoginPage from "@/pages/LoginPage";
 import OnboardingPage from "@/pages/OnboardingPage";
 import DashboardApp from "@/components/DashBoardApp";
 import NotFound from "@/pages/NotFound";
+import DashBoard from "./pages/DashBoard";
+import { Gym } from "./pages/Gym";
+import { Diet } from "./pages/Diet";
+import { Plans } from "./pages/Plans";
+import { Profile } from "./pages/Profile";
+import Announcements from "./pages/Announcements";
 
 const App = () => (
   <TooltipProvider>
@@ -17,7 +23,15 @@ const App = () => (
         <Route path="/" element={<Index />} />
         <Route path="/LoginPage" element={<LoginPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/DashBoard/*" element={<DashboardApp />} />
+        <Route path="/DashBoard" element={<DashboardApp />}>
+          <Route index element={<DashBoard />} />
+          <Route path="Treino" element={<Gym />} />
+          <Route path="Dieta" element={<Diet />} />
+          <Route path="Planos" element={<Plans />} />
+          <Route path="Perfil" element={<Profile />} />
+          <Route path="Announcements" element={<Announcements />} />
+        </Route>
+
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
