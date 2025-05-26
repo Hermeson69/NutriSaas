be5b09c8b9969a0532e6500f2e49b5e943d03980
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-26 13:37:13 -0300

    feat: new dashboard ok

--- src/components/DashBoardApp.tsx
+++ src/components/DashBoardApp.tsx
@@ -1,12 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { SidebarNavigation } from "@/components/Navegation/sidebar-navigation";
-import DashBoard from "@/pages/DashBoard";
-import { Gym } from "@/pages/Gym";
-import { Diet } from "@/pages/Diet";
-import { Plans } from "@/pages/Plans";
-import { Profile } from "@/pages/Profile";
 import { ThemeProvider, useTheme } from "@/components/Provider/theme-provider";
-import Announcements from "@/pages/Announcements";
 import { Layout } from "@/components/Layout/Layout";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { cn } from "@/lib/utils";
@@ -29,14 +23,7 @@ export default function DashboardApp() {
             <ThemeToggle size="md" />
           </div>
           <main className="flex-1 p-5">
-            <Routes>
-              <Route path="/" element={<DashBoard />} />
-              <Route path="Treino" element={<Gym />} />
-              <Route path="Dieta" element={<Diet />} />
-              <Route path="Planos" element={<Plans />} />
-              <Route path="Perfil" element={<Profile />} />
-              <Route path="Announcements" element={<Announcements />} />
-            </Routes>
+            <Outlet />
           </main>
         </Layout>
       </div>
