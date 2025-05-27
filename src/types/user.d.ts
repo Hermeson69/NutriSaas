cbe9778c8f3acabfb6adf7fcec1b6b4ef12857dd
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-27 09:32:29 -0300

    feat: types for user

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,21 +1,38 @@
 declare global {
-    interface TypeUserRequest {
-        name: string;
-        apelido?: string;
-    }
-    interface TypeUserResponse {
-        email: string;
-        name?: string | null;
-        
-    }
-    interface TypeLoginRequest {
-        email: string;
-        password: string;
-    }
-    interface TypeLoginResponse {
-        acess_token: string;
-        user: TypeUserResponse
-    }
+  interface TypeUserRequest {
+    name: string;
+    apelido?: string;
+    sexo: string;
+    telefone: string; 
+    data_de_nascimento: string; 
+    rua: string;
+    bairro: string;
+    numero: number | string;
+    cep: string; 
+    cidade: string;
+    estado: string;
+    peso: number;
+    altura: number;
+    indice_atividade_fisica: string;
+    indice_gordura_corporal: number;
+    circunferencia_pescoco: number;
+    circunferencia_cintura: number;
+    circunferencia_quadril: number;
+    restricao_alimentar: string;
+    meta: boolean;
+  }
+  interface TypeUserResponse {
+    email: string;
+    name?: string | null;
+  }
+  interface TypeLoginRequest {
+    email: string;
+    password: string;
+  }
+  interface TypeLoginResponse {
+    acess_token: string;
+    user: TypeUserResponse;
+  }
 }
 
 export {};
