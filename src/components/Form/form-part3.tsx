a7c5d342ed64dde771b0b59acd409747347c834b
Hermeson Alves <hermesonalves256@gmail.com>
2025-06-03 19:29:49 -0300

    feat: creation of the component for the modularization of the data collection form part3

---
+++ src/components/Form/form-part3.tsx
@@ -0,0 +1,43 @@
+import { Upload } from "lucide-react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Label } from "../ui/label"
+
+export const FormPart3 = () => {
+    return (
+        <Card>
+            <CardHeader>
+              <CardTitle>Cadastro De Informações Do Sistema</CardTitle>
+              <CardDescription>
+                Dados Médicos - Informe seu histórico médico
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="medicalRestriction">
+                  Restrição médica (Opcional)
+                </Label>
+                <select
+                  id="medicalRestriction"
+                  className="w-full p-2 border rounded"
+                >
+                  <option value="">Selecione...</option>
+                  <option value="outra">Outra</option>
+                </select>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="medicalFile">Upload de dados médicos</Label>
+                <div className="flex items-center justify-center w-full">
+                  <label
+                    htmlFor="medicalFile"
+                    className="flex-1 flex items-center justify-center w-full h-10 border-2 border-dashed rounded-lg cursor-pointer bg-muted hover:bg-accent"
+                  >
+                    <Upload className="h-4 w-4 mr-2" />
+                    <span>Escolha um arquivo</span>
+                    <input id="medicalFile" type="file" className="hidden" />
+                  </label>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+    )
+}
