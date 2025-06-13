448900b106959f607bb28e3de1469ac1b661381f
Hermeson Alves <hermesonalves256@gmail.com>
2025-06-13 20:24:43 -0300

    feat: ajust a circule im redirection

--- src/components/loading-redirect.tsx
+++ src/components/loading-redirect.tsx
@@ -19,7 +19,7 @@ export function LoadingRedirect({message = "Redirecionando...."}:LoadingRedirect
 return(
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6">
         <div className="flex flex-col items-center gap-4">
-            <div className="w-16 h-16 bg-green-800 rounded-6xl animate-pulse"></div>
+            <div className="w-16 h-16 bg-green-800 rounded-[50%] animate-pulse"></div>
             <p className="text-lg font-medium text-light">{message}</p>
             <p className="text-sm text-muted-foreground">Agurde um momento...</p>
         </div>
