33cc10f57fa1ef080eeb2eab0a586dcce927dba3
Hermeson Alves <hermesonalves256@gmail.com>
2025-06-22 15:27:52 -0300

    fix: fix a responsabilite for full

--- src/components/Gym/WorkoutSheet.tsx
+++ src/components/Gym/WorkoutSheet.tsx
@@ -34,7 +34,7 @@ export function WorkoutSheet({ workout, onBack }: WorkoutSheetProps) {
   const currentEx = currentWorkout.exercises[currentExercise]
 
   return (
-    <div className="max-w-4xl mx-auto p-4 space-y-6">
+    <div className="max-w-full mx-auto p-4 space-y-6">
       {/* Header */}
       <Card>
         <CardHeader>
