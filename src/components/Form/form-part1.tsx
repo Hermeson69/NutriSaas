5225a66290dd84a33074c88d765ba27eb40fa058
Hermeson Alves <hermesonalves256@gmail.com>
2025-06-25 19:33:07 -0300

    feat: refactored form 1 for send all correct data to the back end

--- src/components/Form/form-part1.tsx
+++ src/components/Form/form-part1.tsx
@@ -5,6 +5,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 import { DatePickerNoCalendar } from "../DatePickerNoCalendar";
 import { useState } from "react";
@@ -33,8 +40,8 @@ export const FormPart1 = () => {
       </CardHeader>
       <CardContent className="grid gap-4 w-full">
         <div className="grid gap-2 w-full">
-          <Label htmlFor="name">Nome:</Label>
-          <Input id="name" placeholder="Seu nome" />
+          <Label htmlFor="name">Nome: <span className="text-red-600">*</span></Label>
+          <Input id="name" placeholder="Seu nome" required/>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid gap-2">
@@ -42,18 +49,27 @@ export const FormPart1 = () => {
             <Input id="surname" placeholder="Seu apelido" />
           </div>
           <div className="grid gap-2">
-            <Label htmlFor="sex">Sexo:</Label>
-            <Input id="sex" placeholder="Seu sexo" />
+            <Label htmlFor="sex">Sexo: <span className="text-red-600">*</span></Label>
+            <Select required> 
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Sexo" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Male">Masculino</SelectItem>
+                <SelectItem value="Famale">Femenino</SelectItem>
+                <SelectItem value="Other">Outro</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-3 gap-3">
           <div className="grid gap-2">
-            <Label htmlFor="phone">Telefone (DDD):</Label>
-            <Input id="phone" placeholder="(11) 99999-9999" />
+            <Label htmlFor="phone">Telefone (DDD): <span className="text-red-600">*</span></Label>
+            <Input id="phone" placeholder="(11) 99999-9999" required />
           </div>
           <div className="grid gap-2">
-            <Label htmlFor="birthDate">Data De Nascimento:</Label>
-            <DatePickerNoCalendar selected={date} onSelect={setDate} />
+            <Label htmlFor="birthDate">Data De Nascimento: <span className="text-red-600">*</span></Label>
+            <DatePickerNoCalendar selected={date} onSelect={setDate}/>
           </div>
           <div className="grid gap-2">
             <Label htmlFor="id">Idade:</Label>
@@ -61,31 +77,31 @@ export const FormPart1 = () => {
           </div>
         </div>
         <div className="grid gap-2">
-          <Label htmlFor="street">Rua:</Label>
-          <Input id="street" placeholder="Sua rua" />
+          <Label htmlFor="street">Rua: <span className="text-red-600">*</span></Label>
+          <Input id="street" placeholder="Sua rua" required />
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid gap-2">
-            <Label htmlFor="neighborhood">Bairro:</Label>
-            <Input id="neighborhood" placeholder="Seu bairro" />
+            <Label htmlFor="neighborhood">Bairro: <span className="text-red-600">*</span></Label>
+            <Input id="neighborhood" placeholder="Seu bairro" required />
           </div>
           <div className="grid gap-2">
-            <Label htmlFor="number">Nº:</Label>
-            <Input id="number" placeholder="Número" />
+            <Label htmlFor="number">Nº: <span className="text-red-600">*</span></Label>
+            <Input id="number" placeholder="Número" required />
           </div>
         </div>
         <div className="grid grid-cols-3 gap-4">
           <div className="grid gap-2">
-            <Label htmlFor="cep">CEP:</Label>
-            <Input id="city" placeholder="CEP" />
+            <Label htmlFor="cep">CEP: <span className="text-red-600">*</span></Label>
+            <Input id="cep" placeholder="CEP" required />
           </div>
           <div className="grid gap-2">
-            <Label htmlFor="city">Cidade:</Label>
-            <Input id="city" placeholder="Sua cidade" />
+            <Label htmlFor="city">Cidade: <span className="text-red-600">*</span></Label>
+            <Input id="city" placeholder="Sua cidade" required />
           </div>
           <div className="grid gap-2">
-            <Label htmlFor="state">Estado:</Label>
-            <Input id="state" placeholder="Seu estado" />
+            <Label htmlFor="state">Estado: <span className="text-red-600">*</span></Label>
+            <Input id="state" placeholder="Seu estado" required />
           </div>
         </div>
       </CardContent>
